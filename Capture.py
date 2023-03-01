9f3efa9f84c0b03fd745699e1b83819e0f9a6e13
Aayush3014 <ayushak3008@gmail.com>
2023-03-02 01:42:49 +0530

    Making Directory for storing images

--- Capture.py
+++ Capture.py
@@ -10,3 +10,21 @@ import os
 # Import time to use sleep() method for taking break in between taking pictures of objects.
 import time
 
+labels = ['thumbsup', 'thumbsdown', 'thankyou', 'livelong']
+
+number_imgs = 5
+
+directory = r"C:\\Users\\ayush\\Downloads\\TensorFlow Object Detection\\Tensorflow\\workspace\\images\\collectedimages"
+
+if not os.path.exists(directory):
+    if os.name == 'posix':
+        os.makedirs(directory,0o777)
+    if os.name == 'nt':
+         os.makedirs(directory,0o777)
+
+
+for label in labels:
+    path = os.path.join(directory, label)
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
